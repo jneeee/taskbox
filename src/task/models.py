afcc0c95725881ceb194fb95e311b5fef89fb262
jneeee <jneeee@qq.com>
2022-11-19 12:25:35 +0800

    fix dynamodb operate

--- src/task/models.py
+++ src/task/models.py
@@ -1,31 +1,37 @@
 import logging
 
-import boto3
+from boto3 import dynamodb
 
 LOG = logging.getLogger(__name__)
 
 
 class Tableclient():
-    def __init__(self, dbname) -> None:
-        dynamodb = boto3.resource('dynamodb')
-        self.table = dynamodb.Table(dbname)
+    def __init__(self, tablename) -> None:
+        self.table = dynamodb.Table(tablename)
 
-    def get(self, key):
-        # key: dict
+    def get(self, item):
+        # item: dict{'id': key}
+        if not isinstance(item, dict):
+            return False
         resp = None
         try:
-            resp = self.table.get_item(Key=key)
+            resp = self.table.get_item(Key=item).get("Item")
         except Exception as e:
             LOG.error(e)
         return resp
 
-    def delete(self, key):
-        self.table.delete_item(key)
+    def delete(self, item):
+        # item: dict{'id': key}
+        if not isinstance(item, dict):
+            return False
+        self.table.delete_item(Item=item)
 
-    def put_item(self, key=None, value=None):
-        if not key or not value:
-            raise ValueError
-        self.table.put_item(key=key, value=value)
+    def put_item(self, item):
+        # item: dict{'id': id, 'value': value}
+        if not isinstance(item, dict):
+            return False
+        self.table.put_item(Item=item)
+        LOG.info(f'Put_item: {item}')
 
 
 def Task(object):
