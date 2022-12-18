fc9b38ab325a0b1502eaab9250e5430edc5c6387
jneeee <jneeee@qq.com>
2022-12-18 17:38:49 +0800

    task: add cronreq

--- src/taskbox/user_task/__init__.py
+++ src/taskbox/user_task/__init__.py
@@ -2,13 +2,13 @@
 Import the task module you need
 
 '''
-from . import taskdemo
+from . import taskcronreq
 from . import hostloc_getpoint
 from . import checkin189
 
 
 __all__ = [
-    'taskdemo',
+    'taskcronreq',
     'hostloc_getpoint',
     'checkin189',
     ]
---
+++ src/taskbox/user_task/taskcronreq.py
@@ -0,0 +1,33 @@
+import requests
+
+from taskbox.taskbase.task import Task
+from taskbox.utils.tools import LOG
+
+
+__all__ = ['CornReq']
+
+class CornReq(Task):
+    '''定时访问一个网址
+    '''
+    name_zh = '测试任务'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def step(self, config):
+        '''这里是任务具体做的事情
+
+        盒子会根据设置的周期，调用这个方法。返回的结果会显示在web的‘结果’一栏。
+        '''
+        res = getattr(requests, config.get('method'))(config.get'url')
+        return f'执行 {config} 成功：{res}'
+
+    def get_conf_list(self):
+        '''method 是 requests支持的请求方法，暂不支持 data/param 字段'''
+        return {
+            'url': '要访问的地址',
+            'method': 'get, option, post',
+        }
+
+
+CornReq.register()
